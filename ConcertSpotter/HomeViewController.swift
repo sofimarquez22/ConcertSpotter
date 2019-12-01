faf3cc09f28364bd051af7606702e89929c1359c
Matthew <malambert@csumb.edu>
2019-12-01 12:41:55 -0800

    Map code

--- ConcertSpotter/HomeViewController.swift
+++ ConcertSpotter/HomeViewController.swift
@@ -7,28 +7,121 @@
 //
 
 import UIKit
+import Firebase
 import MapKit
 import CoreLocation
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var map: MKMapView!
     let locationManager = CLLocationManager()
-    let regionInMeters: Double = 50000
+    let regionInMeters: Double = 5000
+    var ticketInfo = [Ticket]()
+    var checked  = false;
     var lat = 0.0;
     var long = 0.0;
     override func viewDidLoad() {
-         checkLocationServices()
-         /*
-         let locationString = String(lat) + "," + String(long)
-         let baseURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=nloLny4RAVOrwufbVORPtcbFGdxG5QCV&latlong=" + locationString + "&keyword=music"
-         print(baseURL);
-         
-         
-         */
-        // Network request snippet
         
+        guard let locValue: CLLocationCoordinate2D = locationManager.location?.coordinate else { return }
+        print("************************LatLong************************")
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        lat = (locValue.latitude)
+        long = (locValue.longitude)
+        print("************************LatLong************************")
+        checkLocationServices()
+        //ticketInfo = ticketMasterApi.init().request(latlong: "37.785834,-122.406417", genreKey: "music")
+        createMap()
+        
+        
+    }
+    
+    /*
+    @IBAction func logoutAction(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+          print ("Error signing out: %@", signOutError)
         }
         
+    }
+    */
+    
+    func createMap()
+    {
+        let genreKey = "music"
+        let url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=pLOeuGq2JL05uEGrZG7DuGWu6sh2OnMz"
+        let latlong  = String(lat) + "," + String(long)
+        print("************************LatLong2************************")
+        print(latlong)
+        print("************************LatLong2************************")
+        let newUrl = URL(string: url + "&latlong=" + latlong + "&keyword=" + genreKey)!
+                
+                let request = URLRequest(url: newUrl, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+                let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+                let task = session.dataTask(with: request) { (data, response, error) in
+                    // This will run when the network request returns
+                    if let error = error {
+                        print(error.localizedDescription)
+                    } else if let data = data {
+                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                        let t = dataDictionary["_embedded"] as! [String:Any]
+                        let temp = t["events"] as! [[String:Any]]
+                        for event in temp {
+                            
+                            guard let info = event["_embedded"] as? [String:Any] else{return}
+                            guard let venue = info["venues"]  as? [[String:Any]] else{return}
+                            
+                            if let location = venue[0]["location"] as? [String: Any]
+                            {
+                                if let longitude = location["longitude"] as? String
+                                {
+                                    if let latitude = location["latitude"] as? String
+                                    {
+        //                                print(longitude)
+        //                                print(latitude)
+                                        if let range = event["priceRanges"] as? [[String: Any]]
+                                        {
+                                            let b = range[0]["min"] as? Double
+                                            let name = event["name"] as? String
+                                            let url = event["url"] as? String
+                                            let venueName = venue[0]["name"] as? String
+                                            self.ticketInfo.append(Ticket(concertName: name ??  "", longitude: longitude, latitude: latitude, url: url ?? "", venueName: venueName ?? "", minPrice: b ?? 0.0))
+                                        }
+                                    }
+                                }
+                            }
+                           
+                        }
+                        for i in self.ticketInfo
+                        {
+                            print("concert name: ", i.concertName)
+                            print("venue name: ", i.venueName)
+                            print("url: ", i.url)
+                            print("min: " , i.minPrice)
+                            print("longitude: ", i.longitude)
+                            print("latitude: ", i.latitude, "\n")
+                        }
+                        
+                    }
+                    self.createAnnotations()
+                }
+                
+                task.resume()
+
+        
+    }
+        func createAnnotations()
+        {
+            for ticket in ticketInfo{
+                let annotations = MKPointAnnotation()
+                annotations.title = ticket.venueName
+                let lat = Double(ticket.latitude) ?? 0.0
+                let long = Double(ticket.longitude) ?? 0.0
+                annotations.coordinate = CLLocationCoordinate2D(latitude: lat , longitude: long )
+                print(ticket.venueName,",",lat,",",long)
+                map.addAnnotation(annotations)
+            }
+        }
         
         func setupLocationManager() {
             locationManager.delegate = self
@@ -61,24 +154,7 @@ class HomeViewController: UIViewController {
                 map.showsUserLocation = true
                 centerViewOnUserLocation()
                 locationManager.startUpdatingLocation()
-                print("***********************************************")
-                let locationString =  String(lat) + "," + String(long)
-                let url = URL(string: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=nloLny4RAVOrwufbVORPtcbFGdxG5QCV&latlong=" + locationString + "&keyword=music")!
-                let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-                session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
-                let task = session.dataTask(with: url) { (data, response, error) in
-                   if let error = error {
-                      print(error.localizedDescription)
-                   } else if let data = data,
-                      let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                      print(dataDictionary)
-                    
-                      // TODO: Get the posts and store in posts property
-                      // TODO: Reload the table view
-                  }
-                }
-                task.resume()
-                print("***********************************************")
+                
                 break
             case .denied:
                 // Show alert instructing them how to turn on permissions
@@ -99,11 +175,12 @@ class HomeViewController: UIViewController {
         func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
             guard let location = locations.last else { return }
             let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-            print(location.coordinate.longitude , " " ,location.coordinate.latitude)
-            lat = location.coordinate.latitude;
-            long = location.coordinate.longitude;
-            
+            //print(location.coordinate.longitude , " " ,location.coordinate.latitude)
+            lat = location.coordinate.latitude
+            long = location.coordinate.longitude
             map.setRegion(region, animated: true)
+            
+            
         }
         
         
