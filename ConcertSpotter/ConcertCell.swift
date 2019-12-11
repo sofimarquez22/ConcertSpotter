5187be41d32821eb45416df4453b346e6d85365e
sofimarquez22 <sofiamarquez@Sofias-MacBook-Air.local>
2019-12-10 18:11:05 -0800

    hard coded the discover page so that it shows something as opposed to just blank cells

--- ConcertSpotter/ConcertCell.swift
+++ ConcertSpotter/ConcertCell.swift
@@ -8,20 +8,18 @@
 
 import UIKit
 
-class ConcertCell: UITableViewCell {
-
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
+//class ConcertCell: UITableViewCell {
+//
+//    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//}
--- ConcertSpotter/DiscoverViewController.swift
+++ ConcertSpotter/DiscoverViewController.swift
@@ -7,36 +7,40 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class DiscoverViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    @IBOutlet weak var tableView: UITableView!
+class DiscoverViewController: UIViewController{
     
-   var tickerCaller = ticketMasterApi.init()
+    var tickerCaller = ticketMasterApi.init()
+    @IBOutlet weak var concertImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
     
     override func viewDidLoad() {
-//        super.viewDidLoad()
-//        tableView.dataSource = self
-//        tableView.delegate = self
-//
-//
-//                self.tableView.reloadData()
-//            }
-//        }
-//        task.resume()
+        super.viewDidLoad()
+        //self.tableView.reloadData()
+        let imageUrl = URL(string: "https://s1.ticketm.net/dam/a/8c2/1e84234c-695c-4f3f-931a-a3ade43038c2_928391_ARTIST_PAGE_3_2.jpg")
+        concertImage.af_setImage(withURL: imageUrl!)
+        nameLabel.text = "Ariana Grande and Friends"
+        dateLabel.text = "January 20, 2020"
+        locationLabel.text = "Honda Center"
+        
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ConcertCell") as! ConcertCell
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 20
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "ConcertCell") as! ConcertCell
+//        let cell = UITableViewCell()
 //        let concert = concerts[indexPath.row]
 //        let name = concert["name"] as! String
-        
-//        cell.nameLabel!.text = name
-        
-        return cell
-    }
+//
+//        cell.textLabel?.text = "Hello"
+//        let imageUrl = URL(string: )
+//        cell.concertImage.af_setImage(withURL: imageUrl!)
+//        return cell
+//    }
     
 }
